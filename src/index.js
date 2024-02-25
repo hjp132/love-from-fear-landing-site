c9345cdb635590a1f3ce0b7ec353ef6df6c6c3b8
harry <harrypalmer889@gmail.com>
2024-02-25 18:30:04 +0000

    original concept - pre feedback

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 import ReactDOM from 'react-dom'
 import './styles.css'
 import App from './App'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faFacebook, faTwitter, faTiktok } from '@fortawesome/free-brands-svg-icons';
 
 function Overlay() {
   return (
-    <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
-      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate3d(-50%,-50%,0)' }}>
-        {/* <h1 style={{ margin: 0, padding: 0, fontSize: '15em', fontWeight: 500, letterSpacing: '-0.05em' }}>hello</h1> */}
+    <div className='overlay'>
+      <div className='overlay-container'>
 
-        <div style={{display: 'flex', flexDirection: 'column', width: '100%'}}>
-          <div style={{background: '#0b0b0bf0', backdropFilter: "blur(16px)", borderWidth: "2px", borderStyle: 'solid', paddingLeft:'6px'}}>
-            <input type="email" id="email" placeholder='Subscribe' autoCorrect='off' autoCapitalize='off' value={""}
-            style={{background: 'transparent', height: 30, padding: 0, border: 'none', outline: 'none'}}></input>
-          </div>
+        <div className='logo'>
+
+        </div>
+        <div className='border email-container'>
+          <input className='input input-arrow' placeholder='subscribe' type='email' id='email'></input>
+          <span><FontAwesomeIcon icon={faArrowRight} /></span>
+        </div>
+
+        <div className='border socials'>
+          <a className='social-link'>
+            <span className='social-link'><FontAwesomeIcon icon={faFacebook} /></span>
+          </a>
+
+          <a className='social-link'>
+            <span className='social-link'><FontAwesomeIcon icon={faInstagram} /></span>
+          </a>
+
+          <a className='social-link'>
+            <span className='social-link'><FontAwesomeIcon icon={faTwitter}/></span>
+          </a>
+
+          <a className='social-link'>
+            <span className='social-link'><FontAwesomeIcon icon={faTiktok}/></span>
+          </a>
         </div>
       </div>
     </div>
