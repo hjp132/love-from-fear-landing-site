6ea08250c636957967bdd86813512d9887f3c427
Harry Palmer <harrypalmer889@gmail.com>
2024-03-23 16:03:49 +0000

    need to sort loading

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import { useRef } from 'react';
-import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { useState, useRef, Suspense } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Points, PointMaterial, useGLTF } from '@react-three/drei'
+import * as random from 'maath/random/dist/maath-random.esm'
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 0, 5] }}>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <RotatingModel modelPath="./models/logo.obj" />
+    <Suspense>
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Logo />
+        <Stars />
     </Canvas>
-  );
+    </Suspense>
+  )
 }
 
-const RotatingModel = ({ modelPath }) => {
-  const obj = useLoader(OBJLoader, modelPath); // Load the .obj model
-  const ref = useRef();
+function Logo() {
+  const logoObject = useGLTF('./models/logo1.gltf')
 
-  // Apply rotation
+  return (
+    <group>
+      <primitive
+        object={logoObject.scene}
+        scale={0.2}
+        position={ [-2.5, 0, 2.5] }
+        rotation-y={0.3} />
+    </group>
+  )
+}
+
+function Stars(props) {
+  const ref = useRef()
+  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.5 }))
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 20;
-    ref.current.rotation.y -= delta / 20;
-  });
-
-  return <primitive object={obj} ref={ref} />;
-};
+    ref.current.rotation.x -= delta / 20
+    ref.current.rotation.y -= delta / 20
+  })
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled={false} {...props}>
+        <PointMaterial transparent color="#f3f6f4" size={0.01} sizeAttenuation={true} depthWrite={false} />
+      </Points>
+    </group>
+  )
+}
